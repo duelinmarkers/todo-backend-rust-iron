31d49499d3f00186ac1b66be994aae4d82f93a40
John Hume <duelin.markers@gmail.com>
2014-08-17 16:34:32 -0500

    Hello, world! in Iron.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate http;
+extern crate iron;
+extern crate logger;
+
+use std::io::net::ip::Ipv4Addr;
+use iron::{Iron, Chain, Request, Response, Server, Status, Unwind, FromFn};
+
+fn hello_world(_req: &mut Request, res: &mut Response) -> Status {
+    let _ = res.serve(::http::status::Ok, "Hello, world!");
+    Unwind
+}
+
 fn main() {
-    println!("Hello, world!")
+    let mut server: Server = Iron::new();
+    server.chain.link(logger::Logger::new(None));
+    server.chain.link(FromFn::new(hello_world));
+    server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
 }
