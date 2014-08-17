cabb3429b2e7e739e70d940ad401d341330af0b0
John Hume <duelin.markers@gmail.com>
2014-08-17 17:40:33 -0500

    Passes the initial GET request test.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 extern crate http;
 extern crate iron;
 extern crate logger;
+extern crate router;
 
 use std::io::net::ip::Ipv4Addr;
-use iron::{Iron, Chain, Request, Response, Server, Status, Unwind, FromFn};
+use http::method::Options;
+use iron::{Iron, Chain, Request, Response, Server, Status, Continue, Unwind, FromFn};
 
-fn hello_world(_req: &mut Request, res: &mut Response) -> Status {
-    let _ = res.serve(::http::status::Ok, "Hello, world!");
+fn set_cors_headers(req: &mut Request, res: &mut Response) -> Status {
+    let _ = res.headers.insert_raw("access-control-allow-origin".to_string(), b"*");
+    if req.method == Options {
+        let _ = res.headers.insert_raw("access-control-allow-headers".to_string(), b"accept, content-type");
+    }
+    Continue
+}
+
+fn empty_success(_req: &mut Request, res: &mut Response) -> Status {
+    let _ = res.serve(::http::status::Ok, "");
     Unwind
 }
 
 fn main() {
+    let mut router = router::Router::new();
+
+    router.options("/", FromFn::new(empty_success));
+    router.get("/", FromFn::new(empty_success));
+
     let mut server: Server = Iron::new();
     server.chain.link(logger::Logger::new(None));
-    server.chain.link(FromFn::new(hello_world));
+    server.chain.link(FromFn::new(set_cors_headers));
+    server.chain.link(router);
     server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
 }
