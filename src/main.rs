118fcd6b830452b6e85f2aec2721969da2d84d90
John Hume <duelin.markers@gmail.com>
2014-08-17 17:55:34 -0500

    Passes the second test!
    the api root responds to a POST with the todo which was posted to it

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ extern crate router;
 
 use std::io::net::ip::Ipv4Addr;
 use http::method::Options;
+use http::headers::content_type::MediaType;
 use iron::{Iron, Chain, Request, Response, Server, Status, Continue, Unwind, FromFn};
 
 fn set_cors_headers(req: &mut Request, res: &mut Response) -> Status {
     let _ = res.headers.insert_raw("access-control-allow-origin".to_string(), b"*");
     if req.method == Options {
         let _ = res.headers.insert_raw("access-control-allow-headers".to_string(), b"accept, content-type");
+        let _ = res.headers.insert_raw("access-control-allow-methods".to_string(), b"GET,HEAD,POST,DELETE,OPTIONS,PUT,PATCH");
     }
     Continue
 }
@@ -20,11 +22,23 @@ fn empty_success(_req: &mut Request, res: &mut Response) -> Status {
     Unwind
 }
 
+fn echo_todo(req: &mut Request, res: &mut Response) -> Status {
+    println!("{}", req.body);
+    res.headers.content_type = Some(MediaType {
+        type_: "application".to_string(),
+        subtype: "json".to_string(),
+        parameters: vec![]
+    });
+    let _ = res.serve(::http::status::Ok, req.body.clone());
+    Unwind
+}
+
 fn main() {
     let mut router = router::Router::new();
 
     router.options("/", FromFn::new(empty_success));
     router.get("/", FromFn::new(empty_success));
+    router.post("/", FromFn::new(echo_todo));
 
     let mut server: Server = Iron::new();
     server.chain.link(logger::Logger::new(None));
